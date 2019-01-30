8a2d67f8ed07c749a597a41f67f4fca7d7c0aa2f
Hongshuan L <lihsca@gmail.com>
2019-01-30 17:52:19 -0500

    move code from ProjectService to ReportService

--- app/services/ReportService.php
+++ app/services/ReportService.php
@@ -6,19 +6,56 @@ use Phalcon\Di\Injectable;
 
 class ReportService extends Injectable
 {
-    public function genDailyReport()
+    public function getErthmeterReport($year, $month)
     {
+        $dayStart = 1;
+        $dayEnd = 15;
+
+        $projects = $this->projectService->getAll();
+
+        foreach ($projects as $project) {
+            $id = $project->id;
+            $name = $project->name;
+            $erthid = $project->erthmeterId;
+
+            if (strlen($project->erthmeterId) == 0) {
+                continue;
+            }
+
+            echo "$id) $name ($erthid)", EOL;
+
+            for ($day = $dayStart; $day <= $dayEnd; $day++) {
+                $date = sprintf('%d-%02d-%02d', $year, $month, $day);
+
+                $erthmeter = $project->getErthmeter($date);
+                if (!$erthmeter) {
+                    echo "SELECT * FROM erthmeter WHERE recorder_id='$erthid' AND date='$date'", EOL;
+                    continue;
+                }
+
+                for ($hour = 0; $hour < 24; $hour++) {
+                    $start = sprintf('%s %02d:00:00', $date, $hour);
+                    $end   = sprintf('%s %02d:59:59', $date, $hour);
+
+                    $power = $project->getTotalPower($start, $end);
+
+                    $key = 'T'.($hour+1); // T1,T2,T3...T24
+                    $rate = $erthmeter[$key];
+
+                    if ($power + $rate > 0) {
+                        echo "$id) $start $end  $power x $rate = ", $power*$rate, EOL;
+                    }
+                }
+                echo EOL;
+            }
+            echo EOL;
+        }
     }
 
-    public function sendDailyReport()
+    public function sendErthmeterReport()
     {
-    }
-
-    public function genMonthlyReport()
-    {
-    }
-
-    public function sendMonthlyReport()
-    {
+        // $this->getErthmeterReport();
+        // makeExcel
+        // sendMail
     }
 }
