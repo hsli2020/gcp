e6e6fb1e4a1f8505a014156800095735cb2d5b8c
Hongshuan L <lihsca@gmail.com>
2019-01-30 18:54:13 -0500

    send report email

--- app/services/ReportService.php
+++ app/services/ReportService.php
@@ -6,10 +6,36 @@ use Phalcon\Di\Injectable;
 
 class ReportService extends Injectable
 {
+    public function send()
+    {
+        echo "Sending Report ...", EOL;
+
+        $this->log('Start sending daily report');
+
+        $users = [
+            'lihsca@gmail.com',
+        ];
+
+       #$report = $this->getErthmeterReport(2019, 1);
+        $report = '';
+        $filename = $this->generateXls($report);
+        $body = $this->generateHtml($report);
+
+        $today = date('Y-m-d');
+        $subject = "GCP Erthmeter Report ($today)";
+
+        foreach ($users as $user) {
+            $this->sendEmail($user, $subject, $body, $filename);
+        }
+
+        $this->log("Report sent completed.\n");
+    }
+
     public function getErthmeterReport($year, $month)
     {
         $dayStart = 1;
         $dayEnd = 15;
+        $report = [];
 
         $projects = $this->projectService->getAll();
 
@@ -17,6 +43,8 @@ class ReportService extends Injectable
             $id = $project->id;
             $name = $project->name;
             $erthid = $project->erthmeterId;
+
+            $project->totalAmount = 0;
 
             if (strlen($project->erthmeterId) == 0) {
                 continue;
@@ -42,6 +70,8 @@ class ReportService extends Injectable
                     $key = 'T'.($hour+1); // T1,T2,T3...T24
                     $rate = $erthmeter[$key];
 
+                    $project->totalAmount += $power*$rate;
+
                     if ($power + $rate > 0) {
                         echo "$id) $start $end  $power x $rate = ", $power*$rate, EOL;
                     }
@@ -49,13 +79,82 @@ class ReportService extends Injectable
                 echo EOL;
             }
             echo EOL;
+            $report[$id] = $project;
+        }
+
+        return $report;
+    }
+
+    public function generateXls($report)
+    {
+        $suffix = 'test'; //date('Ymd');
+        $filename = BASE_DIR . "/app/logs/Report-$suffix.xlsx";
+        return $filename;
+    }
+
+    public function generateHtml($report)
+    {
+        return '<h1>Report Body</h1>';
+
+        ob_start();
+        $date = date('F d, Y');
+        include("./templates/report.tpl");
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+
+    public function sendEmail($recepient, $subject, $body, $filename = '')
+    {
+        $mail = new \PHPMailer();
+
+        $mail->SMTPOptions = [
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ];
+
+        $today = date('Y-m-d');
+
+#       $mail->SMTPDebug = 3;
+        $mail->isSMTP();
+        $mail->Host = '10.6.200.200';
+        $mail->Port = 25;
+        $mail->SMTPAuth = false;
+        $mail->SMTPSecure = false;
+        $mail->From = "OMS@greatcirclesolar.ca";
+        $mail->FromName = "Great Circle Solar";
+        $mail->addAddress($recepient);
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AltBody = "Please find the Report in attachment.";
+
+        if ($filename) {
+            $mail->addAttachment($filename, basename($filename));
+        }
+
+        if (!$mail->send()) {
+            $this->log("Mailer Error: " . $mail->ErrorInfo);
+        } else {
+            $this->log("Report sent to $recepient.");
         }
     }
 
-    public function sendErthmeterReport()
+    protected function log($str)
     {
-        // $this->getErthmeterReport();
-        // makeExcel
-        // sendMail
+        $filename = BASE_DIR . '/app/logs/report.log';
+
+        if (file_exists($filename) && filesize($filename) > 128*1024) {
+            unlink($filename);
+        }
+
+        $str = date('Y-m-d H:i:s ') . $str . "\n";
+
+        echo $str;
+        error_log($str, 3, $filename);
     }
 }
