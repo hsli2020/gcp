1852664fa95116e21d9f91497a296094935f5661
Hongshuan L <lihsca@gmail.com>
2019-04-06 19:06:05 -0400

    update email list

--- app/services/ReportService.php
+++ app/services/ReportService.php
@@ -31,7 +31,8 @@ class ReportService extends Injectable
 
         $users = [
             'lihsca@gmail.com',
-#           'wsong365@gmail.com',
+            'wsong365@gmail.com',
+            'dmacabales@greatcirclesolar.ca ',
         ];
 
         foreach ($users as $user) {
