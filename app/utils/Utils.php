970cf74284dceda472587d1bf0b6f70b54c3b221
Hongshuan L <lihsca@gmail.com>
2020-03-24 19:20:29 -0400

    changeTimezone & convertTime

--- app/utils/Utils.php
+++ app/utils/Utils.php
@@ -6,3 +6,36 @@ function toLocaltime($timeStr)
     $date->setTimezone(new \DateTimeZone('America/Toronto'));
     return $date->format('Y-m-d H:i:s');
 }
+
+// $now = date('Y-m-d H:i:s');
+// convertTime($now, $from, $to);
+// changeTimezone($now, 'America/Toronto', 'UTC');
+// changeTimezone($now, 'America/Toronto', 'EST');
+
+function changeTimezone($time, $from, $to)
+{
+    $date = new \DateTime($time, new \DateTimeZone($from));
+    $date->setTimezone(new \DateTimeZone($to));
+    return $date->format('Y-m-d H:i:s');
+}
+
+// Another implementation
+function convertTime($time, $from, $to)
+{
+    // $from='UTC';
+    // $to='America/New_York';
+    // $date=date($time);
+
+    $default = date_default_timezone_get();
+
+    date_default_timezone_set($from);
+    $newDatetime = strtotime($time);
+
+    date_default_timezone_set($to);
+    $format = 'Y-m-d H:i:s';
+    $newDatetime = date($format, $newDatetime);
+
+    date_default_timezone_set($default);
+
+    return $newDatetime;
+}
