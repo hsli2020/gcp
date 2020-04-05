8b53a87bd3c9ddebea7ab1e5c853bf67c31b820d
Hongshuan L <lihsca@gmail.com>
2020-04-04 23:24:31 -0400

    app/system/WebRelayQuad.php

---
+++ app/system/WebRelayQuad.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\System;
+
+class WebRelayQuad
+{
+    const ROGERS_HOST = "http://74.198.22.2:9001/state.xml";
+    const BELL_HOST   = "http://184.151.59.10:9001/state.xml";
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * http://74.198.22.2:9001/state.xml
+     * http://184.151.59.10:9001/state.xml
+     *
+     * This will return the following XML page.:
+     *
+     * <?xml version="1.0" encoding="utf-8" ?>
+     * <datavalues>
+     *   <relay1state>0</relay1state>
+     *   <relay2state>1</relay2state>
+     *   <relay3state>1</relay3state>
+     *   <relay4state>1</relay4state>
+     * </datavalues>
+     *
+     * <relayXstate> 0 = Off (coil off)
+     * <relayXstate> 1 = On (coil energized)
+     *
+     * @return
+     *
+     *  Array
+     *  (
+     *      [relay1state] => 0
+     *      [relay2state] => 1
+     *      [relay3state] => 1
+     *      [relay4state] => 1
+     *  )
+     */
+    public function getState()
+    {
+        $res = $this->httpGet('');
+        $xml = simplexml_load_string($res);
+        $json = json_encode($xml);
+        $array = json_decode($json, TRUE);
+        return $array;
+    }
+
+    /**
+     * Turn Relay 1 Off （Stop Generator)
+     *   http://74.198.22.2:9001/state.xml?relay1State=0
+     *   http://184.151.59.10:9001/state.xml?relay1State=0
+     *
+     * Turn Relay 1 On （Start Generator)
+     *   http://74.198.22.2:9001/state.xml?relay1State=1
+     *   http://184.151.59.10:9001/state.xml?relay1State=1
+     *
+     * This WebRelay Model has 4 relays,
+     * we use relay 1 to Start/Stop the Generator.
+     */
+    public function setState($relay, $state)
+    {
+        $res = $this->httpGet("?relay{$relay}State=$state");
+        return $res;
+    }
+
+    public function turnOn($relay)
+    {
+        return $this->setState($relay, 1);
+    }
+
+    public function turnOff($relay)
+    {
+        return $this->setState($relay, 0);
+    }
+
+    protected function httpGet($params)
+    {
+        $hosts = [ self::ROGERS_HOST, self::BELL_HOST ];
+
+        $output = "";
+        foreach ($hosts as $host) {
+            $ch = curl_init();
+
+            $url = $host.$params;
+           #echo $url, PHP_EOL;
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            $output = curl_exec($ch);
+           #var_dump($output);
+            curl_close($ch);
+
+            if ($output) {
+                break;
+            }
+        }
+
+        return $output;
+    }
+}
+
+#$a = new WebRelayQuad();
+#var_dump($a->setState(2, 1));
