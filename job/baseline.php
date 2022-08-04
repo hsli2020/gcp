07fe493ea66eef4e4613f49f7765d3823b7f5d7e
Hongshuan L <lihsca@gmail.com>
2022-08-04 09:38:36 -0400

    recalc all baseline

--- job/baseline.php
+++ job/baseline.php
@@ -2,8 +2,30 @@
 
 include __DIR__ . '/../public/init.php';
 
+#$di = \Phalcon\Di::getDefault();
+#
+#$baselineService = $di->get('baselineService');
+#$baselineService->generateHourlyLoad(); // yesterday
+#$baselineService->generateBaseline(); // yesterday
+
 $di = \Phalcon\Di::getDefault();
+$db = $di->get('db');
+
+$db->execute("TRUNCATE TABLE hourly_load");
+$db->execute("TRUNCATE TABLE baseline_history");
 
 $baselineService = $di->get('baselineService');
-$baselineService->generateHourlyLoad(); // yesterday
-$baselineService->generateBaseline(); // yesterday
+
+echo "Generate Hourly Load\n";
+for ($d=78; $d>0; $d--) {
+    $date = date('Y-m-d', strtotime("-$d days"));
+    echo $date, "\n";
+    $baselineService->generateHourlyLoad($date);
+}
+
+echo "Generate Baseline\n";
+for ($d=30; $d>0; $d--) {
+    $date = date('Y-m-d', strtotime("-$d days"));
+    echo $date, "\n";
+    $baselineService->generateBaseline($date);
+}
