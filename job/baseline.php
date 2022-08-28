43350f330d25dffbf25df01751b666a5d76ae110
Hongshuan L <lihsca@gmail.com>
2022-08-28 12:10:32 -0400

    clean up baseline code

--- job/baseline.php
+++ job/baseline.php
@@ -2,12 +2,6 @@
 
 include __DIR__ . '/../public/init.php';
 
-#$di = \Phalcon\Di::getDefault();
-#
-#$baselineService = $di->get('baselineService');
-#$baselineService->generateHourlyLoad(); // yesterday
-#$baselineService->generateBaseline(); // yesterday
-
 $di = \Phalcon\Di::getDefault();
 $db = $di->get('db');
 
@@ -17,23 +11,6 @@ $db->execute("TRUNCATE TABLE baseline_history");
 $baselineService = $di->get('baselineService');
 
 $date = date('Y-m-d', strtotime('-1 days'));
-$date = '2022-08-15';
 
 $baselineService->generateHourlyLoad($date);
 $baselineService->generateBaseline($date);
-
-#$dates = $baselineService->getValidDates();
-#
-#echo "Generate Hourly Load\n";
-#for ($d=78; $d>0; $d--) {
-#    $date = date('Y-m-d', strtotime("-$d days"));
-#    echo $date, "\n";
-#    $baselineService->generateHourlyLoad($date);
-#}
-#
-#echo "Generate Baseline\n";
-#for ($d=30; $d>0; $d--) {
-#    $date = date('Y-m-d', strtotime("-$d days"));
-#    echo $date, "\n";
-#    $baselineService->generateBaseline($date);
-#}
